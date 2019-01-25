19e94644e8c5cbab5011a8b58e56af2dcfcc2bdf
hayeun7777 <kellyhypark@gmail.com>
2019-01-24 19:35:49 -0800

    submitting deliverable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './ga.png';
+
+import Account from './Account';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="content">
+        <div id="nav">
+          <div id="logo"><img src={logo} alt="General Assembly logo" /></div>
+          <div id="title">Bank of GA</div>
+        </div>
+        <Account name="Checking" />
+        <Account name="Saving" />
+        <div className="clear"></div>
       </div>
     );
   }
